6e4040c273aeb88631ea681ba904212c76c8e575
Mit Patel <patel.m9521@gmail.com>
2025-08-23 02:24:53 +0100

    updated config file with sample image urls

--- webapp/config.py
+++ webapp/config.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 # --- File System Paths ---
+STATIC_FOLDER = Path("static/")
+SAMPLES_FOLDER = STATIC_FOLDER / "samples"
 UPLOAD_FOLDER = Path("uploads/")
 MODELS_FOLDER = Path("models/")
 
@@ -9,6 +11,14 @@ DATASET_URLS = {
     "pizza_steak_sushi_10_percent": "https://github.com/GoJo-Rika/PyTorch-FoodVision-Mini/raw/main/data/pizza_steak_sushi.zip",
     "pizza_steak_sushi_20_percent": "https://github.com/GoJo-Rika/PyTorch-FoodVision-Mini/raw/main/data/pizza_steak_sushi_20_percent.zip",
 }
+
+# --- Sample Image Configuration ---
+SAMPLE_IMAGE_URLS = {
+    "sample_pizza.jpg": "https://raw.githubusercontent.com/GoJo-Rika/datasets/refs/heads/main/PyTorch-FoodVision-Mini/pizza.jpeg",
+    "sample_sushi.jpg": "https://raw.githubusercontent.com/GoJo-Rika/datasets/refs/heads/main/PyTorch-FoodVision-Mini/sushi.jpg",
+    "sample_steak.jpg": "https://raw.githubusercontent.com/GoJo-Rika/datasets/refs/heads/main/PyTorch-FoodVision-Mini/steak.jpg",
+}
+
 CLASS_NAMES = ["pizza", "steak", "sushi"]
 DEFAULT_MODEL = "effnetb0"
 
